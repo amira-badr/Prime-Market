65e94db41a12809d7e4af7591b0e6e92a169cfba
amira-badr <apadr7167@gmail.com>
2026-07-16 01:41:50 +0300

    Final check for stripe

--- api/checkout.js
+++ api/checkout.js
@@ -1,7 +1,7 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
-  // تفعيل الـ CORS
+  // السماح بالاتصال من موقعك
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
